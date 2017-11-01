f17dd1d8765d519ae14a7e26400f34cf4a71294f
DarkLight66 <wence_409@yahoo.com>
2017-10-31 23:37:14 -0400

    Uploaded mod's source

---
+++ NoMoreTombs.cs
@@ -0,0 +1,15 @@
+﻿using Terraria.ModLoader;
+
+namespace NoMoreTombs
+{
+    class NoMoreTombs : Mod
+    {
+        public NoMoreTombs()
+        {
+            Properties = new ModProperties()
+            {
+                Autoload = true
+            };
+        }
+    }
+}
